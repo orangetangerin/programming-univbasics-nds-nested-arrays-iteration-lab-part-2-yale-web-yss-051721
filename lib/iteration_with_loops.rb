f42e5bdf9a04bef188ceafc4accc1b39c6a0f650
Jonathan Yu <jonathan.yu.jwy2@yale.edu>
2021-06-03 00:36:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,24 @@
+require 'pry'
 def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-
+  [17, 18, 22, 26, 31, 35, 38, 40, 43, 45, 49, 52, 55, 61, 60, 59, 55, 49, 45, 38, 32, 30, 24, 19]
+  i = 0
+  min = 0
+  small_array = []
+  while i < src.length do
+    j = 0
+    while j < src[i].length do
+      if j == 0
+        min = src[i][j]
+      else src[i][j] < min
+        min = src[i][j]
+      end
+      j += 1
+      binding.pry
+    end
+    small_array << min
+    i += 1
+  end
+  small_array
 end
